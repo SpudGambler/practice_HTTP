446834b63eafe8613d11b8282c3658110b7f6797
SpudGambler <jaimepalozgm@gmail.com>
2022-03-30 13:21:46 -0500

    Files updated and Mongoose connection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
@@ -6,7 +7,12 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Active port', port));
 
 app.get('/', (req, res) => {
-    res.send('Actividad A Completar Mamaguevo');
+  res.send('Actividad A Completar En La Web Mamaguevo');
 });
+
+mongoose
+  .connect(process.env.MONGODB_STRING_CONNECTION)
+  .then(() => console.log('Success connection with mongo'))
+  .catch((error) => console.error(error));
 
 app.use(express.json());
